a141f0a51c00f16562c33a8bcbd67fc8cc709fcb
John Mettraux <jmettraux@gmail.com>
2008-05-12 17:04:33 +0900

    added TreeChecker test

---
+++ test/treechecker_test.rb
@@ -0,0 +1,71 @@
+
+#
+# Testing OpenWFE
+#
+# John Mettraux at openwfe.org
+#
+# Mon May 12 14:12:54 JST 2008
+#
+
+require 'rubygems'
+
+require 'test/unit'
+
+require 'openwfe/util/treechecker'
+
+#
+# testing expression conditions
+#
+
+class TreeCheckerTest < Test::Unit::TestCase
+
+    #def setup
+    #end
+
+    #def teardown
+    #end
+
+    def _test_0
+
+        assert_safe "1+1"
+        assert_unsafe "exit"
+        assert_unsafe "puts $BATEAU"
+        assert_unsafe "def surf }"
+    end
+
+    def test_1
+
+        #assert_unsafe "abort"
+        #assert_unsafe "abort; puts 'ok'"
+
+        assert_unsafe "puts 'ok'; abort"
+            # this one is weird, it does a stack level too deep
+    end
+
+    protected
+
+        def assert_safe (code)
+
+            assert check(code)
+        end
+
+        def assert_unsafe (code)
+
+            assert (not check(code))
+        end
+
+        def check (code)
+
+            begin
+
+                OpenWFE::TreeChecker.check code
+
+            rescue Exception => e
+                #puts e
+                #puts e.backtrace
+                return false
+            end
+            
+            true
+        end
+end
