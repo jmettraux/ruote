25c8c0adb86260f96f171cb5ff9d00ea6e53ae94
John Mettraux <jmettraux@gmail.com>
2009-01-24 10:56:03 +0900

    new test structure in place

---
+++ test/functional/test.rb
@@ -0,0 +1,13 @@
+
+#
+# testing Ruote
+#
+# since Mon Oct  9 22:19:44 JST 2006
+#
+
+dirpath = File.dirname(__FILE__)
+
+entries = Dir.new(dirpath).entries.select { |e| e.match(/ft\_.*\.rb$/) }.sort
+
+entries.each { |e| load "#{dirpath}/#{e}" }
+
---
+++ test/test.rb
@@ -0,0 +1,10 @@
+
+#
+# testing Ruote
+#
+# since Mon Oct  9 22:19:44 JST 2006
+#
+
+load File.dirname(__FILE__) + '/unit/test.rb'
+load File.dirname(__FILE__) + '/functional/test.rb'
+
