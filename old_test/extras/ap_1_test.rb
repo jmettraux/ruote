90d02846565de5b3dd0b904aa990ef426d49d5b7
John Mettraux <jmettraux@gmail.com>
2009-02-04 15:00:47 +0900

    monkey patch for ar2.2.x

--- old_test/extras/ap_1_test.rb
+++ old_test/extras/ap_1_test.rb
@@ -47,6 +47,31 @@ class Active1Test < Test::Unit::TestCase
     assert_equal 100, wi.fields.size
   end
 
+  #
+  # monkey patching Active Record 2.2.2 to prevent
+  #
+  # exception : You have a nil object when you didn't expect it!
+  # You might have expected an instance of Array.
+  # The error occurred while evaluating nil.-
+  # ./vendor/rails/activerecord/lib/active_record/connection_adapters/abstract_adapter.rb:159:in `decrement_open_transactions'
+  # ,/vendor/rails/activerecord/lib/active_record/transactions.rb:131:in `transaction'
+  # ./vendor/rails/activerecord/lib/active_record/associations/association_collection.rb:136:in `transaction'
+  # ./vendor/rails/activerecord/lib/active_record/associations/association_collection.rb:111:in `<<'
+  # ./vendor/openwfe/extras/participants/activeparticipants.rb:224:in `from_owfe_workitem'
+  #
+  class ActiveRecord::ConnectionAdapters::AbstractAdapter
+
+    # original :
+    #
+    #def decrement_open_transactions
+    #  @open_transactions -= 1
+    #end
+
+    def decrement_open_transactions
+      @open_transactions && @open_transactions -= 1
+    end
+  end
+
   def test_1
 
     fields = { 'alpha' => '0', 'bravo' => 2, 'charly' => :two }
@@ -55,13 +80,15 @@ class Active1Test < Test::Unit::TestCase
       a << Thread.new do
         sleep rand()
         wi = new_wi "participant_#{i}"
+
         wi.save!
+        #wi.save! rescue nil
       end
       a
     end
     threads.each { |t| t.join }
 
-    assert_equal 10, OpenWFE::Extras::Workitem.find(:all).size
+    assert_equal 50, OpenWFE::Extras::Workitem.find(:all).size
   end
 
 end
