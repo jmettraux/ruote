0db4efdb0ae0a78d4eda30a1b3a28311f7b70544
John Mettraux <jmettraux@gmail.com>
2009-03-18 10:52:39 +0900

    something about bug #24471, not a formal fix though (is it a bug ?)

--- test/functional/ft_9_register_participants.rb
+++ test/functional/ft_9_register_participants.rb
@@ -16,7 +16,7 @@ class FtRegisterParticipantsTest < Test::Unit::TestCase
   #
   # some preparation...
 
-  PDEF = OpenWFE.process_definition 'test' do
+  PDEF0 = OpenWFE.process_definition 'test' do
     sequence do
       alpha
       echo '${f:reply}'
@@ -62,14 +62,14 @@ class FtRegisterParticipantsTest < Test::Unit::TestCase
       @tracer << "ok\n"
     end
 
-    assert_trace(PDEF, 'ok')
+    assert_trace(PDEF0, 'ok')
   end
 
   def test_register_class
 
     @engine.register_participant 'alpha', AaParticipant
 
-    assert_trace(PDEF, 'alright')
+    assert_trace(PDEF0, 'alright')
   end
 
   def test_register_class_with_options
@@ -77,14 +77,43 @@ class FtRegisterParticipantsTest < Test::Unit::TestCase
     @engine.register_participant(
       'alpha', BbParticipant, { :message => 'blright' })
 
-    assert_trace(PDEF, 'blright')
+    assert_trace(PDEF0, 'blright')
   end
 
   def test_register_instance
 
     @engine.register_participant 'alpha', CcParticipant.new
 
-    assert_trace(PDEF, 'clright')
+    assert_trace(PDEF0, 'clright')
+  end
+
+  #
+  # about bug #24471
+  #
+  def test_participant_with_underscore
+
+    pdef = OpenWFE.process_definition 'test' do
+      sequence do
+        a_process
+        b_process
+        c_process
+        #participant 'a-process' <-- error : no participant 'a-process'
+        #participant 'b-process' <-- error : no participant 'b-process'
+        participant 'c-process'
+      end
+    end
+
+    @engine.register_participant 'a_process' do |workitem|
+      @tracer << "a\n"
+    end
+    @engine.register_participant :b_process do |workitem|
+      @tracer << "b\n"
+    end
+    @engine.register_participant 'c-process' do |workitem|
+      @tracer << "c\n"
+    end
+
+    assert_trace(pdef, %w{ a b c c }.join("\n"))
   end
 end
 
