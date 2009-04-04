eb756ebd817a75c339e3bee7ee0af2654be69e83
John Mettraux <jmettraux@gmail.com>
2009-04-04 10:35:16 +0800

    ported old 'iterator' test to new test suite
    Signed-off-by: Kenneth Kalmer <kenneth.kalmer@gmail.com>

--- lib/openwfe/expressions/iterator.rb
+++ lib/openwfe/expressions/iterator.rb
@@ -144,62 +144,62 @@ module OpenWFE
 
     protected
 
-      #
-      # Positions the iterator absolutely.
-      #
-      def position_at (workitem, position)
+    #
+    # Positions the iterator absolutely.
+    #
+    def position_at (workitem, position)
 
-        result = {}
+      result = {}
 
-        value = @iteration_list[position]
+      value = @iteration_list[position]
 
-        return nil if (value == nil)
+      return nil if (value == nil)
 
-        if @to_field
-          workitem.attributes[@to_field] = value
-        else
-          result[@to_variable] = value
-        end
-
-        workitem.attributes[ITERATOR_POSITION] = position
-        result[ITERATOR_POSITION] = position
-
-        @iteration_index = position + 1
-
-        result
+      if @to_field
+        workitem.attributes[@to_field] = value
+      else
+        result[@to_variable] = value
       end
 
-      #
-      # Extracts the iteration list from any value.
-      #
-      def extract_iteration_list (raw_list)
+      workitem.attributes[ITERATOR_POSITION] = position
+      result[ITERATOR_POSITION] = position
 
-        if is_suitable_list?(raw_list)
-          raw_list
-        else
-          extract_list_from_string raw_list.to_s
-        end
+      @iteration_index = position + 1
+
+      result
+    end
+
+    #
+    # Extracts the iteration list from any value.
+    #
+    def extract_iteration_list (raw_list)
+
+      if is_suitable_list?(raw_list)
+        raw_list
+      else
+        extract_list_from_string raw_list.to_s
       end
+    end
 
-      #
-      # Returns true if the given instance can be directly
-      # used as a list.
-      #
-      def is_suitable_list? (instance)
+    #
+    # Returns true if the given instance can be directly
+    # used as a list.
+    #
+    def is_suitable_list? (instance)
 
-        (not instance.is_a?(String)) and \
-        instance.respond_to? :[] and \
-        instance.respond_to? :length
-      end
+      (not instance.is_a?(String)) and \
+      instance.respond_to? :[] and \
+      instance.respond_to? :length
+    end
 
-      #
-      # Extracts the list from the string (comma separated list
-      # usually).
-      #
-      def extract_list_from_string (s)
+    #
+    # Extracts the list from the string (comma separated list
+    # usually).
+    #
+    def extract_list_from_string (s)
 
-        s.split @value_separator
-      end
+      s.split @value_separator
+    end
   end
 
 end
