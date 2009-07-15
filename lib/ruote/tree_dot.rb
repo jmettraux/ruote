1bd699451555cc7b49d52546663ce08b5fbbd0f2
John Mettraux <jmettraux@gmail.com>
2009-07-15 10:07:43 +0900

    tree_to_dot, an initial implementation

---
+++ lib/ruote/tree_dot.rb
@@ -0,0 +1,85 @@
+#--
+# Copyright (c) 2005-2009, John Mettraux, jmettraux@gmail.com
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# Made in Japan.
+#++
+
+
+module Ruote
+
+  # Turns a process definition tree to a graphviz dot representation.
+  #
+  # http://www.graphviz.org
+  #
+  def self.tree_to_dot (tree, name='ruote process definition')
+
+    s = "digraph \"#{name}\" {\n"
+    s << branch_to_dot('0', tree).join("\n")
+    s << "\n}\n"
+  end
+
+  protected
+
+  def self.branch_to_dot (expid, exp)
+
+    [
+      "  \"#{expid}\" "+
+      "[ label = \"#{exp[0]} #{exp[1].inspect.gsub("\"", "'")}\" ];"
+    ] +
+    children_to_dot(expid, exp)
+  end
+
+  def self.children_to_dot (expid, exp)
+
+    exp_name = exp[0]
+    child_count = exp[2].size
+
+    i = -1
+
+    a = exp[2].collect do |child|
+      i += 1
+      branch_to_dot("#{expid}_#{i}", child)
+    end
+
+    if child_count > 0 # there are children
+
+      if %w[ concurrence if ].include?(exp_name)
+
+        (0..child_count - 1).each do |i|
+          a << "  \"#{expid}\" -> \"#{expid}_#{i}\";"
+          a << "  \"#{expid}_#{i}\" -> \"#{expid}\";"
+        end
+
+      else
+
+        a << "  \"#{expid}\" -> \"#{expid}_0\";"
+        a << "  \"#{expid}_#{child_count -1}\" -> \"#{expid}\";"
+
+        (0..child_count - 2).each do |i|
+          a << "  \"#{expid}_#{i}\" -> \"#{expid}_#{i + 1}\";"
+        end
+      end
+    end
+
+    a
+  end
+end
+
---
+++ test/unit/ut_8_tree_to_dot.rb
@@ -0,0 +1,72 @@
+
+#
+# Testing Ruote
+#
+# Wed Jul 15 09:27:20 JST 2009
+#
+
+require File.dirname(__FILE__) + '/../test_helper.rb'
+
+require 'ruote/parser/ruby_dsl'
+require 'ruote/tree_dot'
+
+
+class TreeDotTest < Test::Unit::TestCase
+
+  def test_sequence
+
+    tree = Ruote.define :name => 'test' do
+      sequence do
+        alpha
+        bravo
+      end
+    end
+
+    #puts Ruote.tree_to_dot(tree)
+
+    assert_equal(
+      %{
+digraph "ruote process definition" {
+  "0" [ label = "define {'name'=>'test'}" ];
+  "0_0" [ label = "sequence {}" ];
+  "0_0_0" [ label = "alpha {}" ];
+  "0_0_1" [ label = "bravo {}" ];
+  "0_0" -> "0_0_0";
+  "0_0_1" -> "0_0";
+  "0_0_0" -> "0_0_1";
+  "0" -> "0_0";
+  "0_0" -> "0";
+}
+      }.strip,
+      Ruote.tree_to_dot(tree).strip)
+  end
+
+  def test_concurrence
+
+    tree = Ruote.define :name => 'test' do
+      concurrence do
+        alpha
+        bravo
+      end
+    end
+
+    #puts Ruote.tree_to_dot(tree)
+
+    assert_equal(
+      %{
+digraph "ruote process definition" {
+  "0" [ label = "define {'name'=>'test'}" ];
+  "0_0" [ label = "concurrence {}" ];
+  "0_0_0" [ label = "alpha {}" ];
+  "0_0_1" [ label = "bravo {}" ];
+  "0_0" -> "0_0_0";
+  "0_0_0" -> "0_0";
+  "0_0" -> "0_0_1";
+  "0_0_1" -> "0_0";
+  "0" -> "0_0";
+  "0_0" -> "0";
+}
+      }.strip,
+      Ruote.tree_to_dot(tree).strip)
+  end
+end
