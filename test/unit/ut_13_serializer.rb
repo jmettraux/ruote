70ebdc5d5988f94f8500d2a823ec1ce5d15b2487
John Mettraux <jmettraux@gmail.com>
2009-08-27 11:21:21 +0900

    serializer idea

---
+++ test/unit/ut_13_serializer.rb
@@ -0,0 +1,65 @@
+
+#
+# Testing Ruote
+#
+# Thu Aug 27 10:44:44 JST 2009
+#
+
+require File.join(File.dirname(__FILE__), '..', 'test_helper.rb')
+
+require 'ruote/util/serializer'
+
+
+class SerializerTest < Test::Unit::TestCase
+
+  class Vehicle
+    attr_accessor :type
+    def initialize (t)
+      @type = t
+    end
+  end
+
+  #def setup
+  #end
+  #def teardown
+  #end
+
+  def test_yaml
+
+    s = Ruote::Serializer.new(:yaml)
+
+    data = s.encode(Vehicle.new('bike'))
+
+    assert_match /^---/, data
+
+    v = s.decode(data)
+
+    assert_equal Vehicle, v.class
+    assert_equal 'bike', v.type
+  end
+
+  def test_marshal
+
+    s = Ruote::Serializer.new(:marshal)
+
+    data = s.encode(Vehicle.new('bike'))
+
+    v = s.decode(data)
+
+    assert_equal Vehicle, v.class
+    assert_equal 'bike', v.type
+  end
+
+  def test_marshal64
+
+    s = Ruote::Serializer.new(:marshal64)
+
+    data = s.encode(Vehicle.new('bike'))
+
+    v = s.decode(data)
+
+    assert_equal Vehicle, v.class
+    assert_equal 'bike', v.type
+  end
+end
+
