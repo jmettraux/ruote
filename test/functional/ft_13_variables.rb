4746b94d23cef0172f1e6ee853b8827240db2cde
John Mettraux <jmettraux@gmail.com>
2009-09-01 15:06:39 +0900

    making sure process root variables behave correctly

--- test/functional/ft_13_variables.rb
+++ test/functional/ft_13_variables.rb
@@ -17,6 +17,8 @@ class FtVariablesTest < Test::Unit::TestCase
       echo 'at:${wfid}:${expid}'
     end
 
+    #noisy
+
     wfid = @engine.launch(pdef)
     wait_for(wfid)
 
@@ -41,6 +43,28 @@ class FtVariablesTest < Test::Unit::TestCase
       1, logger.log.select { |e| e[0] == :variables && e[1] == :set }.size)
     assert_equal(
       1, logger.log.select { |e| e[0] == :variables && e[1] == :unset }.size)
+  end
+
+  def test_process_root_variables
+
+    pdef = Ruote.process_definition do
+      sequence do
+        set 'v:va' => 'a0'
+        set 'v:/vb' => 'b0'
+        echo '${v:va}:${v:vb}:${v:/va}:${v:/vb}'
+        sub0
+        echo '${v:va}:${v:vb}:${v:/va}:${v:/vb}'
+      end
+      define 'sub0' do
+        set 'v:va' => 'a1'
+        set 'v:/vb' => 'b1'
+        echo '${v:va}:${v:vb}:${v:/va}:${v:/vb}'
+      end
+    end
+
+    #noisy
+
+    assert_trace(pdef, %w[ a0:b0:a0:b0 a1:b1:a0:b1 a0:b1:a0:b1 ])
   end
 end
 
