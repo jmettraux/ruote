71aff9c41a9c7a82aaf5590ce44e2fb8c732689d
John Mettraux <jmettraux@gmail.com>
2009-09-14 08:42:59 +0900

    added test from missing :ref when break/rewind/jump/...

---
+++ test/functional/eft_25_command.rb
@@ -0,0 +1,28 @@
+
+#
+# Testing Ruote (OpenWFEru)
+#
+# Mon Sep 14 08:39:35 JST 2009
+#
+
+require File.join(File.dirname(__FILE__), 'base')
+
+
+class EftCommandTest < Test::Unit::TestCase
+  include FunctionalBase
+
+  def test_ref_to_missing_tag
+
+    pdef = Ruote.process_definition :name => 'test' do
+      sequence do
+        _break :ref => 'nemo'
+        echo 'done.'
+      end
+    end
+
+    #noisy
+
+    assert_trace pdef, 'done.'
+  end
+end
+
