2dc34c6fda189843e17c0d5eb0cbd4c7a6de0856
John Mettraux <jmettraux@gmail.com>
2009-10-08 14:21:27 +0900

    --split trick for [jruby] testing

--- test/functional/test.rb
+++ test/functional/test.rb
@@ -5,18 +5,30 @@
 # since Mon Oct  9 22:19:44 JST 2006
 #
 
+def l (t)
+
+  if ARGV.include?('--split')
+    puts
+    puts "=== #{t} :"
+    puts `ruby #{t}`
+    exit $?.exitstatus if $?.exitstatus != 0
+  else
+    load(t)
+  end
+end
+
 
 # TODO : rft_ as well...
 
-Dir.glob(File.join(File.dirname(__FILE__), 'ct_*.rb')).sort.each { |t| load(t) }
+Dir.glob(File.join(File.dirname(__FILE__), 'ct_*.rb')).sort.each { |t| l(t) }
   # concurrence/collision tests, tests about 2+ instances of ruote colliding
 
-Dir.glob(File.join(File.dirname(__FILE__), 'ft_*.rb')).sort.each { |t| load(t) }
+Dir.glob(File.join(File.dirname(__FILE__), 'ft_*.rb')).sort.each { |t| l(t) }
   # functional tests targetting features rather than expressions
 
-Dir.glob(File.join(File.dirname(__FILE__), 'rt_*.rb')).sort.each { |t| load(t) }
+Dir.glob(File.join(File.dirname(__FILE__), 'rt_*.rb')).sort.each { |t| l(t) }
   # restart tests, start sthing, stop engine, restart, expect thing to resume
 
-Dir.glob(File.join(File.dirname(__FILE__), 'eft_*.rb')).sort.each { |t| load(t) }
+Dir.glob(File.join(File.dirname(__FILE__), 'eft_*.rb')).sort.each { |t| l(t) }
   # functional tests targetting specifing expressions
 
