201e50147350ccfa12cc4d23aebdf6c11ca220c3
John Mettraux <jmettraux@gmail.com>
2009-12-18 22:10:22 +0900

    tiny bm test

---
+++ test/bm/seq_thousand.rb
@@ -0,0 +1,31 @@
+
+#
+# testing ruote
+#
+# Fri Dec 18 19:19:07 JST 2009
+#
+
+require File.join(File.dirname(__FILE__), '..', 'functional', 'base')
+
+
+class BmSeqThousandTest < Test::Unit::TestCase
+  include FunctionalBase
+
+  def test_empty_sequence
+
+    n = 100
+
+    pdef = Ruote.process_definition :name => 'test' do
+      sequence do
+        iterator :on => (1..n).to_a do
+          echo 'a'
+        end
+      end
+    end
+
+    noisy
+
+    assert_trace pdef, [ 'a' ] * n
+  end
+end
+
