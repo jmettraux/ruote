b4385e7eba009a041035af00de7eeccb00523bb5
John Mettraux <jmettraux@gmail.com>
2010-02-04 13:26:34 +0900

    preparing for ruote-dm

--- test/unit/storages.rb
+++ test/unit/storages.rb
@@ -11,3 +11,7 @@ puts "\n\n\n== couch_storage"
 puts
 puts `ruby -r patron -r yajl test/unit/ut_17_storage.rb --couch`
 
+puts "\n\n\n== dm_storage"
+puts
+puts `ruby -r yajl test/unit/ut_17_storage.rb --dm`
+
