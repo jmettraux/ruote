16f9e9384f1b51b8346d3bf23c3e978123dd2570
John Mettraux <jmettraux@gmail.com>
2010-04-30 18:36:12 +0900

    a helper for running concurrent tests

---
+++ test/functional/crunner.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+TEST="test/functional/ct_0_concurrence.rb"
+if [ $1 = "1" ]; then
+  TEST="test/functional/ct_1_iterator.rb"
+fi
+if [ $1 = "2" ]; then
+  TEST="test/functional/ct_2_cancel.rb"
+fi
+
+COUNT=0
+
+while [ $? == 0 ]
+do
+  echo " *** $COUNT"
+  ((COUNT=$COUNT + 1))
+  ruby $TEST $1 $2
+done
+
