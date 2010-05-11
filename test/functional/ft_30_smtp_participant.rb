0fe7eab9b5ad9743e00df0be5cca2a553849b390
John Mettraux <jmettraux@gmail.com>
2010-05-11 14:01:37 +0900

    hopefully, hardened ft_30_stmp test

--- test/functional/ft_30_smtp_participant.rb
+++ test/functional/ft_30_smtp_participant.rb
@@ -24,49 +24,50 @@ class NftSmtpParticipantTest < Test::Unit::TestCase
     end
   end
 
-  def _test_smtp
-
-    return if Ruote::JAVA
-
-    pdef = Ruote.process_definition :name => 'test' do
-      sequence do
-        set 'f:item' => 'cat food'
-        alpha
-      end
-    end
-
-    trapfile = Ruote::WIN ? 'ruote_mailtrap.txt' : '/tmp/ruote_mailtrap.txt'
-    FileUtils.rm_f(trapfile)
-
-    t = Thread.new do
-      Trap.new('127.0.0.1', 2525, true, trapfile)
-    end
-    sleep 0.040
-      # give it some time to start listening
-
-    @engine.register_participant(
-      :alpha,
-      Ruote::SmtpParticipant.new(
-        :server => '127.0.0.1',
-        :port => 2525,
-        :to => 'toto@cloudwhatever.ch',
-        :from => 'john@outoftheblue.ch',
-        :notification => true,
-        :template => %{
-  Hello, do you want ${f:item} ?
-        }))
-
-    #noisy
-
-    wfid = @engine.launch(pdef)
-
-    sleep 0.450
-
-    assert_match(/cat food/, File.read(trapfile))
-    assert_nil @engine.process(wfid)
-
-    t.kill
-  end
+#  def test_smtp
+#
+#    return if Ruote::JAVA
+#
+#    pdef = Ruote.process_definition :name => 'test' do
+#      sequence do
+#        set 'f:item' => 'cat food'
+#        alpha
+#      end
+#    end
+#
+#    trapfile = Ruote::WIN ? 'ruote_mailtrap.txt' : '/tmp/ruote_mailtrap.txt'
+#    FileUtils.rm_f(trapfile)
+#
+#    t = Thread.new do
+#      Trap.new('127.0.0.1', 2525, true, trapfile)
+#    end
+#    sleep 0.040
+#      # give it some time to start listening
+#
+#    @engine.register_participant(
+#      :alpha,
+#      Ruote::SmtpParticipant.new(
+#        :server => '127.0.0.1',
+#        :port => 2525,
+#        :to => 'toto@cloudwhatever.ch',
+#        :from => 'john@outoftheblue.ch',
+#        :notification => true,
+#        :template => %{
+#  Hello, do you want ${f:item} ?
+#        }))
+#
+#    #noisy
+#
+#    wfid = @engine.launch(pdef)
+#
+#    #sleep 0.450
+#    wait_for(wfid)
+#
+#    assert_match(/cat food/, File.read(trapfile))
+#    assert_nil @engine.process(wfid)
+#
+#    t.kill
+#  end
 
   def test_smtp_non_instance_participant
 
@@ -104,9 +105,10 @@ class NftSmtpParticipantTest < Test::Unit::TestCase
 
     wfid = @engine.launch(pdef)
 
-    sleep 0.450
+    #sleep 0.450
+    wait_for(wfid)
 
-    assert_match(/cat food/, File.read(trapfile))
+    assert_match(/want cat food/, File.read(trapfile))
     assert_nil @engine.process(wfid)
 
     t.kill
