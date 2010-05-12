e4d62670fbee975df5a54d5e083a4149f5d3e3c5
John Mettraux <jmettraux@gmail.com>
2010-05-12 21:40:27 +0900

    color helpers for test/unit/storages.rb as well

--- test/unit/storages.rb
+++ test/unit/storages.rb
@@ -1,31 +1,37 @@
 
 dashdash = `ruby -v`.match(/^ruby 1\.9\./) ? '' : '--'
 
-puts "\n\n\n== in memory"
+puts("\n\n[32m== in memory[0m")
 puts
 puts `ruby test/unit/storage.rb`
+puts("\n[41mFAILED[0m") if $?.exitstatus.to_i != 0
 
-puts "\n\n\n== fs"
+puts("\n\n[32m== fs[0m")
 puts
 puts `ruby test/unit/storage.rb #{dashdash} --fs`
+puts("\n[41mFAILED[0m") if $?.exitstatus.to_i != 0
 
-puts "\n\n\n== ruote-couch"
+puts("\n\n[32m== route-couch[0m")
 puts
 #puts `ruby -r patron -r yajl test/unit/storage.rb #{dashdash} --couch`
 puts `ruby test/unit/storage.rb #{dashdash} --couch`
+puts("\n[41mFAILED[0m") if $?.exitstatus.to_i != 0
 
-puts "\n\n\n== ruote-dm"
+puts("\n\n[32m== route-dm[0m")
 puts
 #puts `ruby -r yajl test/unit/storage.rb #{dashdash} --dm`
 puts `ruby test/unit/storage.rb #{dashdash} --dm`
+puts("\n[41mFAILED[0m") if $?.exitstatus.to_i != 0
 
-puts "\n\n\n== ruote-redis"
+puts("\n\n[32m== route-redis[0m")
 puts
 #puts `ruby -r yajl test/unit/storage.rb #{dashdash} --redis`
 puts `ruby test/unit/storage.rb #{dashdash} --redis`
+puts("\n[41mFAILED[0m") if $?.exitstatus.to_i != 0
 
-puts "\n\n\n== ruote-beanstalk"
+puts("\n\n[32m== route-beanstalk[0m")
 puts
 #puts `ruby -r yajl test/unit/storage.rb #{dashdash} --beanstalk`
 puts `ruby test/unit/storage.rb #{dashdash} --beanstalk`
+puts("\n[41mFAILED[0m") if $?.exitstatus.to_i != 0
 
