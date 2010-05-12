36756f92860c3feea6aaf44595757137d073351e
John Mettraux <jmettraux@gmail.com>
2010-05-12 20:55:42 +0900

    test/unit/storages.rb now OK with 1.9.x and 1.8.x

--- test/unit/storages.rb
+++ test/unit/storages.rb
@@ -1,21 +1,32 @@
 
+dashdash = ''
+dashdash = '--' unless `ruby -v`.match(/^ruby 1\.9\./)
+
 puts "\n\n\n== in memory"
 puts
 puts `ruby test/unit/storage.rb`
 
-puts "\n\n\n== fs_storage"
+puts "\n\n\n== fs"
 puts
-puts `ruby test/unit/storage.rb --fs`
+puts `ruby test/unit/storage.rb #{dashdash} --fs`
 
-puts "\n\n\n== couch_storage"
+puts "\n\n\n== ruote-couch"
 puts
-puts `ruby -r patron -r yajl test/unit/storage.rb --couch`
+#puts `ruby -r patron -r yajl test/unit/storage.rb #{dashdash} --couch`
+puts `ruby test/unit/storage.rb #{dashdash} --couch`
 
-puts "\n\n\n== dm_storage"
+puts "\n\n\n== ruote-dm"
 puts
-puts `ruby -r yajl test/unit/storage.rb --dm`
+#puts `ruby -r yajl test/unit/storage.rb #{dashdash} --dm`
+puts `ruby test/unit/storage.rb #{dashdash} --dm`
 
-puts "\n\n\n== redis_storage"
+puts "\n\n\n== ruote-redis"
 puts
-puts `ruby -r yajl test/unit/storage.rb --redis`
+#puts `ruby -r yajl test/unit/storage.rb #{dashdash} --redis`
+puts `ruby test/unit/storage.rb #{dashdash} --redis`
+
+puts "\n\n\n== ruote-beanstalk"
+puts
+#puts `ruby -r yajl test/unit/storage.rb #{dashdash} --beanstalk`
+puts `ruby test/unit/storage.rb #{dashdash} --beanstalk`
 
