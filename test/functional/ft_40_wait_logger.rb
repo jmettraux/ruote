a2877f878b35691f735a3e215c0ddce2cde4fd7a
John Mettraux <jmettraux@gmail.com>
2010-07-13 23:24:21 +0900

    ft_40 fixed

--- test/functional/ft_40_wait_logger.rb
+++ test/functional/ft_40_wait_logger.rb
@@ -12,49 +12,49 @@ require 'ruote/part/storage_participant'
 
 class FtWaitLoggerTest < Test::Unit::TestCase
 
+  def teardown
+
+    @engine.shutdown
+    @engine.context.storage.purge!
+  end
+
   def test_wait_logger
 
-    assert true; return
-    # TODO : engine cleanup !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    @engine = Ruote::Engine.new(Ruote::Worker.new(determine_storage({})))
 
-    engine = Ruote::Engine.new(Ruote::Worker.new(determine_storage({})))
-
-    sp = engine.register_participant :alpha, Ruote::StorageParticipant
+    sp = @engine.register_participant :alpha, Ruote::StorageParticipant
 
     pdef = Ruote.process_definition { alpha }
 
-    wfid = engine.launch(pdef)
+    wfid = @engine.launch(pdef)
 
     sleep 0.500
 
     sp.reply(sp.first)
 
-    r = engine.wait_for(wfid)
+    r = @engine.wait_for(wfid)
 
     assert_equal 'terminated', r['action']
   end
 
   def test_wait_logger_seen
 
-    assert true; return
-    # TODO : engine cleanup !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    @engine = Ruote::Engine.new(Ruote::Worker.new(determine_storage({})))
 
-    engine = Ruote::Engine.new(Ruote::Worker.new(determine_storage({})))
-
-    #engine.context.logger.noisy = true
+    #@engine.noisy = true
 
     pdef = Ruote.process_definition { }
 
-    wfid = engine.launch(pdef)
+    wfid = @engine.launch(pdef)
 
     sleep 0.500
 
-    assert_equal 2, engine.context.logger.instance_variable_get(:@seen).size
+    assert_equal 2, @engine.context.logger.instance_variable_get(:@seen).size
 
-    r = engine.wait_for(wfid)
+    r = @engine.wait_for(wfid)
 
     assert_equal 'terminated', r['action']
-    assert_equal 0, engine.context.logger.instance_variable_get(:@seen).size
+    assert_equal 0, @engine.context.logger.instance_variable_get(:@seen).size
   end
 end
 
