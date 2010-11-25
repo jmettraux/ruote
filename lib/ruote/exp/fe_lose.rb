8182f8fc73b18c359a36a8996213671cc598a71e
John Mettraux <jmettraux@gmail.com>
2010-11-25 11:09:14 +0900

    implemented the 'lose' expression
    Thanks Claudio for reminding me about it

---
+++ lib/ruote/exp/fe_lose.rb
@@ -0,0 +1,83 @@
+#--
+# Copyright (c) 2005-2010, John Mettraux, jmettraux@gmail.com
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# Made in Japan.
+#++
+
+
+module Ruote::Exp
+
+  #
+  # Never replies to its parent expression. Simply applies its first child,
+  # if any, and just sits there.
+  #
+  # When cancelled, cancels its child (if any).
+  #
+  # In this example, the reminding sequence never replies to the concurrence.
+  # The concurrence is only over when "alfred" replies.
+  #
+  #   Ruote.process_definition do
+  #     concurrence :count => 1 do
+  #       alfred
+  #       lose do
+  #         sequence do
+  #           wait '2d'
+  #           send_reminder_to_alfred
+  #           wait '2h'
+  #           send_alarm_to_boss
+  #         end
+  #       end
+  #     end
+  #   end
+  #
+  # Maybe shorter :
+  #
+  #   Ruote.process_definition do
+  #     concurrence :count => 1 do
+  #       alfred
+  #       sequence do
+  #         wait '2d'
+  #         send_reminder_to_alfred
+  #         wait '2h'
+  #         send_alarm_to_boss
+  #         lose
+  #       end
+  #     end
+  #   end
+  #
+  # 'lose' on its own acts like a dead-end.
+  #
+  class LoseExpression < FlowExpression
+
+    names :lose
+
+    def apply
+
+      apply_child(0, h.applied_workitem)
+    end
+
+    def reply (workitem)
+
+      # never gets called
+    end
+  end
+end
+
---
+++ test/functional/eft_32_lose.rb
@@ -0,0 +1,93 @@
+
+#
+# testing ruote
+#
+# Thu Nov 25 10:05:28 JST 2010
+#
+
+require File.join(File.dirname(__FILE__), 'base')
+
+require 'ruote/participant'
+
+
+class EftLoseTest < Test::Unit::TestCase
+  include FunctionalBase
+
+  def test_lose_alone
+
+    pdef = Ruote.process_definition do
+      lose
+    end
+
+    #@engine.noisy = true
+
+    wfid = @engine.launch(pdef)
+
+    @engine.wait_for(2)
+      # wait until the process reaches the 'lose' expression
+
+    sleep 0.500
+
+    ps = @engine.process(wfid)
+
+    assert_equal 0, ps.errors.size
+    assert_equal 2, ps.expressions.size
+
+    # the process is just stuck at the 'lose' expression
+  end
+
+  def test_losing_child
+
+    pdef = Ruote.process_definition do
+      lose do
+        alpha
+      end
+      charly
+    end
+
+    @engine.register_participant '.+' do |wi|
+      @tracer << wi.participant_name
+    end
+
+    #@engine.noisy = true
+
+    wfid = @engine.launch(pdef)
+
+    @engine.wait_for(7)
+      # wait until 'alpha' replies to its parent 'lose'
+
+    sleep 0.500
+
+    assert_equal 'alpha', @tracer.to_s
+  end
+
+  def test_cancelling_lose
+
+    pdef = Ruote.process_definition do
+      lose do
+        alpha
+      end
+    end
+
+    @engine.register_participant '.+', Ruote::StorageParticipant
+
+    #@engine.noisy = true
+
+    wfid = @engine.launch(pdef)
+
+    @engine.wait_for(6)
+      # wait until 'alpha' replies to its parent 'lose'
+
+    sleep 0.500
+
+    assert_equal 1, @engine.storage_participant.size
+
+    @engine.cancel_process(wfid)
+
+    sleep 0.500
+
+    assert_equal 0, @engine.storage_participant.size
+    assert_nil @engine.process(wfid)
+  end
+end
+
