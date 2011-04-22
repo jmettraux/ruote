cb53b4d986e7d15da3b802a7f056caefbe60459e
John Mettraux <jmettraux@gmail.com>
2011-04-22 16:17:46 +0900

    gathering var indirection stuff in one test case

--- test/functional/ft_27_var_indirection.rb
+++ test/functional/ft_27_var_indirection.rb
@@ -11,6 +11,25 @@ require File.join(File.dirname(__FILE__), 'base')
 class FtVarIndirectionTest < Test::Unit::TestCase
   include FunctionalBase
 
+  def test_var_alias
+
+    pdef = Ruote.process_definition :name => 'def0' do
+      set :v => 'alpha', :val => 'bravo'
+      sequence do
+        alpha
+        bravo
+      end
+    end
+
+    alpha = @engine.register_participant :bravo do |workitem|
+      @tracer << "b:#{workitem.fields['params']['original_ref']}\n"
+    end
+
+    #noisy
+
+    assert_trace(%w[ b:alpha b: ], pdef)
+  end
+
   def test_participant_indirection
 
     pdef = Ruote.process_definition do
