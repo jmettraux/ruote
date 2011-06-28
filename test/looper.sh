986d628154fe838f3cf82137259f60ef45a27369
John Mettraux <jmettraux@gmail.com>
2011-06-28 22:45:25 +0900

    stopper for test/looper.sh

--- test/looper.sh
+++ test/looper.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
 # runs a test in a loop, until it fails
+#
+# the last argument can be an integer, the max number of tests.
 
 COUNT=0
+echo $BASH_ARGV
 
 while [ $? == 0 ]
 do
+
   echo
   echo " *** $COUNT"
   ((COUNT=$COUNT + 1))
   time ruby -I. $*
+
+  if [[ "$COUNT" == $BASH_ARGV ]]; then
+    break
+  fi
 done
 
