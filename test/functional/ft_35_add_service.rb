e68f127b6b768ff8fee8218bed099e8591b8ed6f
John Mettraux <jmettraux@gmail.com>
2011-08-04 09:22:36 +0900

    newly registered history gets messages.
    Closes gh-28

--- test/functional/ft_35_add_service.rb
+++ test/functional/ft_35_add_service.rb
@@ -68,5 +68,39 @@ class FtAddServiceTest < Test::Unit::TestCase
 
     assert_equal Ruote::StorageHistory, engine.context.history.class
   end
+
+  # Fighting https://github.com/jmettraux/ruote/issues/28
+  #
+  def test_add_history
+
+    assert_equal Ruote::DefaultHistory, @engine.history.class
+
+    @engine.add_service(
+      'history', 'ruote/log/storage_history', 'Ruote::StorageHistory')
+
+    assert_equal Ruote::StorageHistory, @engine.history.class
+
+    engine = Ruote::Engine.new(@engine.storage)
+
+    assert_equal Ruote::StorageHistory, engine.history.class
+  end
+
+  # Fighting https://github.com/jmettraux/ruote/issues/28
+  #
+  def test_add_history_and_log
+
+    #@engine.noisy = true
+
+    #previous_history = @engine.history
+
+    @engine.add_service(
+      'history', 'ruote/log/storage_history', 'Ruote::StorageHistory')
+
+    @engine.storage.put_msg('noop', 'nada' => true)
+
+    sleep 0.500
+
+    assert_equal 'noop', @engine.history.by_date(Time.now.to_s).first['action']
+  end
 end
 
