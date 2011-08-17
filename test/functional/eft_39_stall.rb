b65045dc378eaf164b2097fd276d2957c09b3239
John Mettraux <jmettraux@gmail.com>
2011-08-17 09:01:14 +0900

    'stall' expression (mostly for testing)

---
+++ test/functional/eft_39_stall.rb
@@ -0,0 +1,35 @@
+
+#
+# testing ruote
+#
+# Wed Aug 17 08:29:13 JST 2011
+#
+# Santa Barbara
+#
+
+require File.join(File.dirname(__FILE__), 'base')
+
+
+class EftStallTest < Test::Unit::TestCase
+  include FunctionalBase
+
+  def test_no_operation
+
+    pdef = Ruote.define do
+      stall
+    end
+
+    #@engine.noisy = true
+
+    wfid = @engine.launch(pdef)
+
+    sleep 0.750
+
+    ps = @engine.ps(wfid)
+
+    assert_equal 0, ps.errors.size
+    assert_equal 2, ps.expressions.size
+    assert_equal 'stall', ps.expressions.last.name
+  end
+end
+
