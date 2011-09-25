3061e0ccbfc6527512ea5b485755d886b93fe7c6
John Mettraux <jmettraux@gmail.com>
2011-09-25 09:14:11 +0900

    ft_40: "wait_for" over "sleep"

--- test/functional/ft_40_wait_logger.rb
+++ test/functional/ft_40_wait_logger.rb
@@ -10,6 +10,9 @@ require File.expand_path('../base', __FILE__)
 require 'ruote/part/storage_participant'
 
 
+# Storage dependent WaitLogger tests. For storage independent test see
+# test/unit/ut_3_wait_logger.rb
+#
 class FtWaitLoggerTest < Test::Unit::TestCase
 
   def teardown
@@ -42,14 +45,26 @@ class FtWaitLoggerTest < Test::Unit::TestCase
 
     @engine = Ruote::Engine.new(Ruote::Worker.new(determine_storage({})))
 
+    counter = Object.new
+    class << counter
+      attr_reader :count
+      def on_msg(msg)
+        @count = (@count || 0) + 1
+      end
+      def wait_for(i)
+        loop { sleep 0.100; break if @count >= i }
+      end
+    end
+
+    @engine.add_service('counter', counter)
+
     #@engine.noisy = true
 
     pdef = Ruote.process_definition { }
 
     wfid = @engine.launch(pdef)
 
-    sleep 0.500
-    sleep 2.000 if @engine.storage.class.name.match(/Couch/)
+    counter.wait_for(2)
 
     assert_equal 2, @engine.context.logger.instance_variable_get(:@seen).size
 
