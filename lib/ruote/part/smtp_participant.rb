d353f0c5d153c2f46793b40e654421d30e6209a7
John Mettraux <jmettraux@gmail.com>
2012-12-24 10:48:45 +0900

    2013

--- lib/ruote/part/smtp_participant.rb
+++ lib/ruote/part/smtp_participant.rb
@@ -1,5 +1,5 @@
 #--
-# Copyright (c) 2005-2012, Alain Hoang and John Mettraux.
+# Copyright (c) 2005-2013, Alain Hoang and John Mettraux.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
