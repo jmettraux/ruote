4482968bd154fd221ddd2efbf7935ba29dc04167
John Mettraux <jmettraux@gmail.com>
2013-01-19 17:51:02 +0900

    forget the test USR1 and USR2 traps on windows

--- test/functional/signals.rb
+++ test/functional/signals.rb
@@ -5,7 +5,6 @@
 # Sat Sep 20 23:40:10 JST 2008
 #
 
-
 # Hitting CTRL-C reveals the fancy dashboard log (if any) and the
 # current backtrace.
 #
@@ -27,76 +26,79 @@ trap 'INT' do
 end #if RUBY_VERSION.match(/^1.9./)
 
 
-# Didn't use it much, pops a console while tests are running.
-#
-trap 'USR1' do
+unless Ruote::WIN
 
-  require 'irb'
-  require 'irb/completion'
+  # Didn't use it much, pops a console while tests are running.
+  #
+  trap 'USR1' do
 
-  IRB.setup(nil)
-  ws = IRB::WorkSpace.new(binding)
-  irb = IRB::Irb.new(ws)
-  IRB::conf[:MAIN_CONTEXT] = irb.context
-  irb.eval_input
-end
+    require 'irb'
+    require 'irb/completion'
 
-
-# USR2 is used for CI timeouts. Tries to print a max of useful information
-# and then exits.
-#
-trap 'USR2' do
-
-  # for CI timeouts
-
-  begin
-
-    puts
-    puts '-' * 80
-    puts *$_engine.context.logger.fancy_log if $_engine
-
-    puts
-    puts '-' * 80
-    if defined?(MiniTest)
-      ObjectSpace.each_object(
-        MiniTest::Unit
-      ).first.instance_eval do
-        self.status
-        puts
-        @report.each_with_index do |msg, i|
-          @@out.puts "\n%3d) %s" % [i + 1, msg]
-        end
-      end
-    elsif defined?(Test::Unit::UI::Console::TestRunner)
-      ObjectSpace.each_object(
-        Test::Unit::UI::Console::TestRunner
-      ).first.instance_eval do
-        finished(-1)
-      end
-    else
-      puts "no test/unit or MiniTest"
-    end
-
-    puts '-' * 80
-    puts "threads: #{Thread.list.size}"
-    Thread.list.each do |t|
-      puts '-' * 80
-      if Thread.current.respond_to?(:backtrace) # only >= 1.9.2p290 it seems
-        puts "thread backtrace:"
-        puts *t.backtrace
-      else
-        t.exit unless t == Thread.main
-      end
-    end
-
-    puts '-' * 80
-
-  rescue Exception => e
-    p e
-    puts *e.backtrace
+    IRB.setup(nil)
+    ws = IRB::WorkSpace.new(binding)
+    irb = IRB::Irb.new(ws)
+    IRB::conf[:MAIN_CONTEXT] = irb.context
+    irb.eval_input
   end
 
-  exit 1
+
+  # USR2 is used for CI timeouts. Tries to print a max of useful information
+  # and then exits.
+  #
+  trap 'USR2' do
+
+    # for CI timeouts
+
+    begin
+
+      puts
+      puts '-' * 80
+      puts *$_engine.context.logger.fancy_log if $_engine
+
+      puts
+      puts '-' * 80
+      if defined?(MiniTest)
+        ObjectSpace.each_object(
+          MiniTest::Unit
+        ).first.instance_eval do
+          self.status
+          puts
+          @report.each_with_index do |msg, i|
+            @@out.puts "\n%3d) %s" % [i + 1, msg]
+          end
+        end
+      elsif defined?(Test::Unit::UI::Console::TestRunner)
+        ObjectSpace.each_object(
+          Test::Unit::UI::Console::TestRunner
+        ).first.instance_eval do
+          finished(-1)
+        end
+      else
+        puts "no test/unit or MiniTest"
+      end
+
+      puts '-' * 80
+      puts "threads: #{Thread.list.size}"
+      Thread.list.each do |t|
+        puts '-' * 80
+        if Thread.current.respond_to?(:backtrace) # only >= 1.9.2p290 it seems
+          puts "thread backtrace:"
+          puts *t.backtrace
+        else
+          t.exit unless t == Thread.main
+        end
+      end
+
+      puts '-' * 80
+
+    rescue Exception => e
+      p e
+      puts *e.backtrace
+    end
+
+    exit 1
+  end
 end
 
 puts "pid #{$$}"
