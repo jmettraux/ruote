69c338af5a53248c6c097a3be1896ea6de95a6d8
John Mettraux <jmettraux@gmail.com>
2013-04-10 13:18:40 +0900

    add test for @northox

--- test/functional/ft_76_observer.rb
+++ test/functional/ft_76_observer.rb
@@ -36,9 +36,10 @@ class FtObserverTest < Test::Unit::TestCase
       #
       # using an instantiated service for easier testing...
 
-    pdef = Ruote.define do
-      alpha
-    end
+    pdef =
+      Ruote.define do
+        alpha
+      end
 
     wfid = @dashboard.launch(pdef)
     m = @dashboard.wait_for(wfid)
@@ -48,6 +49,33 @@ class FtObserverTest < Test::Unit::TestCase
     assert_equal(
       [ [ 'launch', wfid ], [ 'terminated', wfid ] ],
       my_observer.events)
+  end
+
+  class MyOtherObserver < Ruote::Observer
+    attr_reader :wi
+    def on_pre_msg_receive(msg)
+      @wi =
+        Ruote::Exp::FlowExpression.fetch(@context, msg['fei']).applied_workitem
+    end
+  end
+
+  # For
+  # https://groups.google.com/forum/#!topic/openwferu-users/_-sanN8B9q8
+  #
+  def test_on_receive_and_applied_workitem
+
+    @dashboard.register 'alpha', Ruote::NoOpParticipant
+
+    my_other_observer = MyOtherObserver.new(@dashboard.context)
+    @dashboard.add_service 'my_other_observer', my_other_observer
+      #
+      # using an instantiated service for easier testing...
+
+    wfid = @dashboard.launch(Ruote.define { alpha })
+    m = @dashboard.wait_for(wfid)
+
+    assert_equal Ruote::Workitem, my_other_observer.wi.class
+    assert_equal '0_0', my_other_observer.wi.fei.expid
   end
 end
 
